1bab632bef36447aaa77db781158b8d11e105766
Jonas Lindberg <eksno@protonmail.com>
2025-02-22 17:06:09 +0700

    build(vite): configure allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -73,6 +73,9 @@ const gitInfo = getGitInfo();
 
 export default defineConfig((config) => {
   return {
+    server: {
+      allowedHosts: ['bolt.starti.no'],
+    },
     define: {
       __COMMIT_HASH: JSON.stringify(gitInfo.commitHash),
       __GIT_BRANCH: JSON.stringify(gitInfo.branch),
